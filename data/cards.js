9a24e2b7a271ccd437959f2dbb66996772c9be22
alkplima <alkplima@gmail.com>
2023-12-06 20:14:04 -0300

    fix: imgs

--- data/cards.js
+++ data/cards.js
@@ -4,14 +4,14 @@ const cards = {
     description: `A análise do filme Wall-E, sob a perspectiva da semiótica de Greimas, abre uma janela para uma interpretação mais profunda das temáticas subjacentes de isolamento, saúde mental e solidão.
 
     O filme retrata um mundo pós-apocalíptico em que a humanidade abandonou a Terra, deixando-a inabitável e coberta por detritos. Nesse cenário desolador, o protagonista, um pequeno robô catador de lixo chamado Wall-E, simboliza a persistência solitária da vida em um planeta abandonado.`,
-    image: '../public/images/intro.png'
+    image: './images/intro.png'
   },
   2: {
     title: '1. Isolamento',
     description: `A história de Wall-E se desenrola em um futuro distópico, no qual os humanos deixaram a Terra devido à poluição extrema. A solidão de Wall-E reflete não apenas a ausência de outros seres, mas também a desconexão do ser humano com o meio ambiente.
 
     Sob a perspectiva semiótica, o cenário deserto e a rotina repetitiva criam visualmente um contexto que transmite a monotonia e o isolamento. Sendo assim, o ambiente repleto de pilhas de lixo é um signo que representa também a solidão.`,
-    image: '../public/images/isolamento.png'
+    image: './images/isolamento.png'
   },
   3: {
     title: '2. Saúde mental',
@@ -20,14 +20,14 @@ const cards = {
     Através da lente semiótica, esses objetos constituem signos que o personagem utiliza para elaborar um sistema simbólico que compense a ausência de interações sociais.
     
     Por meio desses signos a saúde mental é explorada, mostrando a necessidade humana básica de conexão e significado na vida.`,
-    image: '../public/images/saude.png'
+    image: './images/saude.png'
   },
   4: {
     title: '3. Solidão',
     description: `A dinâmica do filme muda quando Eva, um robô de busca, chega na Terra. O foco se torna então a relação entre Wall-E e Eva. Inicialmente, a comunicação entre eles é não verbal, por meio de gestos e expressões, o que pode ser interpretado como uma tentativa de superar a barreira da solidão e da falta de compreensão.
 
     Aqui a semiótica destaca a importância dos signos não verbais na expressão emocional e na construção de relações, enfatizando novamente a necessidade fundamental de conexão humana.`,
-    image: '../public/images/solidao.png'
+    image: './images/solidao.png'
   },
   5: {
     title: '4. Transformações simbólicas',
@@ -36,14 +36,14 @@ const cards = {
     As cadeiras flutuantes simbolizam a desconexão e a dependência tecnológica, enquanto a ausência de comunicação interpessoal reflete a deterioração das relações sociais.
     
     A semiótica de Greimas destaca esses elementos como signos que comunicam uma transformação negativa da sociedade humana.`,
-    image: '../public/images/transformacoes.png'
+    image: './images/transformacoes.png'
   },
   6: {
     title: 'Conclusão',
     description: `Em suma, a análise semiótica de Greimas proporciona uma visão profunda das camadas simbólicas em "Wall-E", explorando temas como isolamento, saúde mental e solidão através dos signos presentes na narrativa e na construção visual do filme.
 
     Essa abordagem permite uma compreensão mais abrangente das mensagens subjacentes que vão além da superfície da história e do senso comum.`,
-    image: '../public/images/conclusao.png'
+    image: './images/conclusao.png'
   }
 }
 
